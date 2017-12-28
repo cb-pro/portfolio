21c3a74559d71d0868e5acdc672f7af5b6217179
eChris <echris@eChriss-Mac-mini.local>
2017-12-28 22:46:30 +0100

    first commit

---
+++ src/components/Home-id-one.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="home-id-one">
+    <div v-html="this.controller.importedContent" />
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      controller: {
+        importedContent: this.$store.state.content
+      }
+    }
+  },
+  created () {
+    // console.log(this.$store.state.content)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .home-id-one {
+    font-family: helvetica, arial, sans-serif;
+    color: #303030
+  }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <HomeIdOne />
+  </div>
+</template>
+
+<script>
+import HomeIdOne from '../components/Home-id-one'
+
+export default {
+  components: {
+    'HomeIdOne': HomeIdOne
+  }
+}
+</script>
+
+<style lang="css">
+</style>
